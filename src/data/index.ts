0fd832e8372f6be966e331eb5009a66e4f20dc66
itayost <itayost1@gmail.com>
2025-05-29 15:01:53 +0300

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,5 @@
 // Export all data
 export { projects } from './projects';
-export { skillsData } from './skills';
-export { resumeData } from './resume';
 
-// Re-export data from constants if needed
+// Re-export data from constants
 export { SKILLS_DATA, RESUME_DATA } from '@/lib/constants/skills';
