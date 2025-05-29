c55ccdc4de5496a4d41695daec69d0e421f5bfc9
itayost <itayost1@gmail.com>
2025-05-29 14:50:51 +0300

    Update MotionButton.tsx

--- src/components/common/MotionButton/MotionButton.tsx
+++ src/components/common/MotionButton/MotionButton.tsx
@@ -1,31 +1,12 @@
 "use client";
 
-import { forwardRef } from "react";
-import { motion, HTMLMotionProps } from "framer-motion";
-import { getButtonStyles, ButtonVariant, ButtonSize } from "../Button/styles";
+import { motion } from "framer-motion";
+import Button from "../Button";
 
-export interface MotionButtonProps extends HTMLMotionProps<"button"> {
-  variant?: ButtonVariant;
-  size?: ButtonSize;
-  fullWidth?: boolean;
-}
-
-const MotionButton = motion(
-  forwardRef<HTMLButtonElement, MotionButtonProps>(
-    ({ variant, size, fullWidth, className, children, ...props }, ref) => {
-      return (
-        <button
-          ref={ref}
-          className={getButtonStyles({ variant, size, fullWidth, className })}
-          {...props}
-        >
-          {children}
-        </button>
-      );
-    }
-  )
-);
+// Create MotionButton by wrapping the existing Button component
+const MotionButton = motion(Button);
 
 MotionButton.displayName = "MotionButton";
 
 export default MotionButton;
+export type { ButtonProps as MotionButtonProps } from "../Button";
